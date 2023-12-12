f85fca47b09b77612f066a3b9bac7ea46c03bacb
Steve Grunwell <233836+stevegrunwell@users.noreply.github.com>
2023-12-11 20:28:51 -0500

    Force the PHPStan job to install a version of PHPUnit

--- src/Exceptions/SelectorException.php
+++ src/Exceptions/SelectorException.php
@@ -2,6 +2,9 @@
 
 namespace SteveGrunwell\PHPUnit_Markup_Assertions\Exceptions;
 
+/**
+ * Base exception for selector-related issues.
+ */
 class SelectorException extends \InvalidArgumentException
 {
 }
